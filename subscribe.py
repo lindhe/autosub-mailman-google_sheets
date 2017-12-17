747d350e54564570b589811ff9d407ec301dbb1e
Andreas Lindhé <andreas@lindhe.io>
2017-12-17 15:00:38 +0100

    Create outline of subscription program

--- subscribe.py
+++ subscribe.py
@@ -5,9 +5,37 @@
 # Author: Andreas Lindhé
 
 import sys
+from bs4 import BeautifulSoup as bs
+import requests as r
+
+# Returns the response object after loggin in
+# TODO: Check that login was successful
+def login(password, domain, mailinglist):
+  url = 'http://lists.' + domain + '/mailman/admin/' + mailinglist
+  credentials = {'adminpw': password, 'admlogin': 'Let me in...'}
+  res = r.post(url, credentials)
+  return (res)
+
+def goto_members():
+  # open members management
+  # open add new member
+  return (0)
+
+def subscribe():
+  sub_page = membership_admin()
+  token = get_csrf(sub_page)
+  return (0)
+
+def get_csrf(sub_page):
+  return (0)
+
+def logout():
+  return (0)
 
 def main():
-  print("Main program")
+  login()
+  subscribe()
+  logout()
   return (0)
 
 
